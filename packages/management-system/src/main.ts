2d5ea7c61775da0996574eedfc628b610a239a13
Nguyen Anh Hoang <nguyenanhhoang.ict@gmail.com>
2019-02-22 23:25:42 +0700

    Add ElementUI, Axios, English locale

--- packages/management-system/src/main.ts
+++ packages/management-system/src/main.ts
@@ -2,11 +2,22 @@ import Vue from 'vue';
 import App from './App.vue';
 import router from '@/router';
 import store from '@/store';
+import ElementUI from 'element-ui';
+import 'element-ui/lib/theme-chalk/index.css';
+import { i18n, loadLanguageAsync } from '@/locales';
 
 Vue.config.productionTip = false;
+Vue.use(ElementUI);
+
+// loadLanguageAsync
+router.beforeEach((to, from, next) => {
+  const lang = to.params.lang || 'en';
+  loadLanguageAsync(lang).then(() => next());
+});
 
 new Vue({
   router,
   store,
-  render: (h) => h(App),
+  i18n,
+  render: h => h(App)
 }).$mount('#app');
