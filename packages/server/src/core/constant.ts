fb3d110979e40e8904eb33d5fe70c24fa5b67368
Nguyen Anh Hoang <nguyenanhhoang.ict@gmail.com>
2019-03-20 19:59:55 +0700

    update constants

--- packages/server/src/core/constant.ts
+++ packages/server/src/core/constant.ts
@@ -3,15 +3,17 @@ import dotenv from 'dotenv';
 dotenv.load();
 
 // ROUTES
+// ----ROOT
 export const ROOT = '/api/v1';
+// ----USER
 export const USER = ROOT + '/user';
 export const USER_REGISTER = USER + '/register';
 export const USER_LOGIN = USER + '/login';
 export const USER_UPDATE_PROFILE = USER + '/update-profile';
+// ----CATEGORY
 export const CATEGORY = ROOT + '/category';
 export const CREATE_CATEGORY = CATEGORY + '/create';
-export const GENERAL = ROOT + '/general';
-export const CONVERT_BASE64_TO_IMG = GENERAL + '/convert-base64-to-img';
+
 
 // ENV
 export const JWT_CHARS = process.env.JWT_CHARS;
