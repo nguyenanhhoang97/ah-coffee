c906090ba5f59e2da1707923495e8f2844159035
Nguyen Anh Hoang <nguyenanhhoang.ict@gmail.com>
2019-05-11 13:17:47 +0700

    Permisson for sidebar

--- packages/management-system/src/components/AppSidebar/index.vue
+++ packages/management-system/src/components/AppSidebar/index.vue
@@ -5,9 +5,9 @@
         <a href="javascript:void(0)" class="simple-text logo-mini">AH</a>
         <a href="javascript:void(0)" class="simple-text logo-normal">Coffee</a>
       </div>
-      <ul class="nav">
+      <ul class="nav" v-if="menus">
         <li class v-for="tab in menus" :key="tab.route">
-          <router-link :to="tab.route" class="nav-link" active-class="active">
+          <router-link :to="tab.route" class="nav-link" active-class="active" v-show="tab.roles">
             <i :class="tab.icon"/>
             <p>{{ tab.name }}</p>
           </router-link>
@@ -19,63 +19,68 @@
 
 <script lang="ts">
 import { Component, Vue } from 'vue-property-decorator';
+import { mapState, mapGetters, mapActions } from 'vuex';
 
-@Component
+@Component({
+  computed: {
+    ...mapState('session', ['role'])
+  }
+})
 export default class AppSidebar extends Vue {
   public menus: any = [
     {
       icon: 'tim-icons icon-chart-pie-36',
       name: 'Dashboard',
       route: '/',
-      roles: ['admin', 'manager', 'salesperson']
+      roles: ['admin', 'manager', 'salesperson'].indexOf(this.$store.state.session.role) > -1
     },
     {
       icon: 'tim-icons icon-puzzle-10',
       name: 'OrderBoard',
       route: '/orderboard',
-      roles: ['admin', 'manager', 'salesperson']
+      roles: ['admin', 'manager', 'salesperson'].indexOf(this.$store.state.session.role) > -1
     },
     {
       icon: 'tim-icons icon-app',
       name: 'Manage Category',
       route: '/category',
-      roles: ['admin', 'manager']
+      roles: ['admin', 'manager'].indexOf(this.$store.state.session.role) > -1
     },
     {
       icon: 'tim-icons icon-atom',
       name: 'Manage Product',
       route: '/product',
-      roles: ['admin', 'manager']
+      roles: ['admin', 'manager'].indexOf(this.$store.state.session.role) > -1
     },
     {
       icon: 'tim-icons icon-notes',
       name: 'Manage Bill',
       route: '/bill',
-      roles: ['admin', 'manager']
+      roles: ['admin', 'manager'].indexOf(this.$store.state.session.role) > -1
     },
     {
       icon: 'tim-icons icon-notes',
       name: 'My Bill',
       route: '/my-bill',
-      roles: ['admin', 'manager']
+      roles: ['admin', 'manager'].indexOf(this.$store.state.session.role) > -1
     },
     {
       icon: 'tim-icons icon-single-02',
       name: 'Manage Customer',
       route: '/customer',
-      roles: ['admin', 'manager', 'salesperson']
+      roles: ['manager', 'salesperson'].indexOf(this.$store.state.session.role) > -1
     },
     {
       icon: 'tim-icons icon-single-02',
       name: 'Manage Salesperson',
       route: '/salesperson',
-      roles: ['admin', 'manager']
+      roles: ['manager'].indexOf(this.$store.state.session.role) > -1
     },
     {
       icon: 'tim-icons icon-single-02',
       name: 'Manage User',
       route: '/user',
-      roles: ['admin', 'manager', 'salesperson']
+      roles: ['admin', 'manager', 'salesperson'].indexOf(this.$store.state.session.role) > -1
     }
   ];
 }
